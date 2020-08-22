0b046272a37a17f3e03db0d39aeeca0bac9a8fda
Peter Wang <pwang649@usc.edu>
2020-08-22 14:27:39 +0800

    Add hello_world.cpp with some code

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+//
+//  hello_world.cpp
+//  
+//
+//  Created by Peter Wang on 8/22/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+}
